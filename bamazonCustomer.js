36a95c166d4ad21933cb8d10878174a705274b91
jasonfarthing <jasonfarthingisnow@gmail.com>
2017-11-30 18:50:05 -0800

    working store

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,61 +12,100 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
 
-
 });
-
+console.log("------------------------------------")
 inquirer.prompt([
     {
         type: 'list',
         name: 'choice',
         message: "Welcome to Bamazon, Ready to start shopping?",
         choices: ["YES", "NO"]
-
-    }
+    } 
 ]).then(function (answers) {
-    console.log(answers);
+    
     if (answers.choice == "YES") {
-        inquirer.prompt([               //need to show available products here
-            {
-                type: 'input',
-                name: 'items',
-                message: "Input a title"
-            },
-            {
-                type: 'input',
-                name: 'category',
-                message: "Input category"
-            },
-            {
-                type: 'input',
-                name: 'price',
-                message: "Input price"
-            }]).then(function (answer) {
-                var items = answer.items;
-                var category = answer.category;
-                var price = answer.price;
-                createProduct(items, category, price);
-            
-            });
+
+        queryAllProducts();//need to show available products here
+
+    }else{
+        console.log("------------------------------------")
+        console.log("Maybe next time, Thanks for Visiting!")
+        console.log("------------------------------------")
+        connection.end();
     }
 
 }).catch(function (err) {
     console.log(err)
 });
-//---------
 
-function createProduct(items, category, price) {
-    console.log("Inserting a new product...\n");
+//--------- update product
+
+function updateProduct(itemId, qnty) {
+    console.log("==============================================\n");
     var query = connection.query(
-        "INSERT INTO items SET ?",
+      "UPDATE products SET ? WHERE ?",
+      [
         {
-            items: items,
-            category: category,
-            price: price
+        stock_quantity: qnty
         },
-        function (err, res) {
-            
-            
+        
+        {
+        id: itemId
         }
-    )
+      ],
+      function(err, res) {
+        
+      }
+    );
+  
+    console.log(query.sql);
+  }
+
+//----------
+//my function to show all the products to the customer
+function queryAllProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log("-----------------------------------");            
+            console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
+    
+        inquirer.prompt([               
+            {
+                type: 'input',
+                name: 'items',
+                message: "Please enter the product ID to continue."
+            }]).then(function (answer) {      
+                yourProduct(answer.items);//use id (ans) to query item from db
+                             //display item
+                             //prompt for amt to buy
+            });
+    });
+};
+
+function yourProduct(product) {
+    connection.query("SELECT * FROM products WHERE id=?", product, function (err, res) {
+        var itemId = res[0].id;
+        var oldQnty = res[0].stock_quantity;
+    
+            console.log("-----------------------------------");            
+            console.log(res[0].id + " | " + res[0].product_name + " | " + res[0].department_name + " | " + res[0].price + " | " + res[0].stock_quantity);
+    
+        console.log("-----------------------------------");
+        inquirer.prompt([               
+            {
+                type: 'input',
+                name: 'items',
+                message: "Please enter the quantity to purchase."
+            }]).then(function (answer) {  
+                var newQnty = oldQnty - answer.items;  
+            console.log("==============================================");     
+            console.log("Congrats! You have bought it, The quantity has been updated."); 
+            updateProduct(itemId, newQnty);
+            //update item qnty based on how many user bought
+            //display item line with new info qnty
+            connection.end();
+            });
+
+    });
 };
